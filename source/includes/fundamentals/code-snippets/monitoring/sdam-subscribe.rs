28f2162450ee912217a76b0c5e6057d673e5c79f
norareidy <nora.reidy@mongodb.com>
2024-06-07 16:10:57 -0400

    DOCSP-39853: Update monitoring code

--- source/includes/fundamentals/code-snippets/monitoring/sdam-subscribe.rs
+++ source/includes/fundamentals/code-snippets/monitoring/sdam-subscribe.rs
@@ -1,35 +1,24 @@
-use std::{ env, sync::Arc };
-
-use bson::Document;
 use mongodb::{
-    Client,
-    Collection,
+    bson::{doc, Document},
+    Client, Collection,
+    event::EventHandler,
     event::sdam::{ SdamEventHandler, ServerOpeningEvent },
     options::ClientOptions,
 };
 
-fn main() -> mongodb::error::Result<()> {
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()> {
     let uri = "<connection string>";
-
-    let mut client_options = ClientOptions::parse_async(uri).await?;
-
+    
     // begin-sdam
-    struct ServerOpenHandler;
-
-    impl SdamEventHandler for ServerOpenHandler {
-        fn handle_server_opening_event(&self, event: ServerOpeningEvent) {
-            eprintln!("Server opening: {:?}", event);
-        }
-    }
-
-    let handler: Arc<dyn SdamEventHandler> = Arc::new(ServerOpenHandler);
-    client_options.sdam_event_handler = Some(handler);
-
+    let mut client_options = ClientOptions::parse(uri).await?;
+    client_options.sdam_event_handler = Some(EventHandler::callback(|ev| println!("{:?}", ev)));
+    
     let client = Client::with_options(client_options)?;
 
     // ... perform actions with the client to generate events
 
     // end-sdam
-
+    
     Ok(())
 }
